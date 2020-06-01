241af8b00ff863fc04a75eaa0ae172fab9203e86
sujood <sujoodsarawan@gmail.com>
2020-06-02 02:17:53 +0300

    Updates

---
+++ precache-manifest.78cdcd705d4441202f200dbc824a7adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8135732a70c535d24e2d1cbccf48093c",
+    "url": "/memployeSystem/index.html"
+  },
+  {
+    "revision": "2d78c71cb84928af4c3c",
+    "url": "/memployeSystem/static/css/main.7fca8bd8.chunk.css"
+  },
+  {
+    "revision": "cca5a5fd93b461744195",
+    "url": "/memployeSystem/static/js/2.88967edf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memployeSystem/static/js/2.88967edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d78c71cb84928af4c3c",
+    "url": "/memployeSystem/static/js/main.c3e9e762.chunk.js"
+  },
+  {
+    "revision": "5cbafafb8a8bde9ab14a",
+    "url": "/memployeSystem/static/js/runtime-main.b6b0743a.js"
+  }
+]);
